a9ef54225e8a274e0cac3351f6c845c77a3249e5
Thomas Hertog <thomas.hertog@external.toyota-europe.com>
2025-06-20 01:03:49 +0200

    tweaking a11y, fixing focus and visual information on custom tip

--- script.js
+++ script.js
@@ -15,10 +15,16 @@ const calculate = function(bill, tip, people) {
     };
 }
 
-const getInfo = function(){
+const getInfo = function(event){
     const form = document.getElementById("tip-calculator");
+    let tip = new FormData(form).get('tip');
+    if(tip === null || tip === 'custom'){
+        tip = parseFloat(document.getElementById('tip-custom-input').value) / 100;
+    }else{
+        document.getElementById('tip-custom-input').value = '';
+    }
     const formData = {bill: new FormData(form).get('bill'),
-                     tip: new FormData(form).get('tip'),
+                     tip,
                      people: new FormData(form).get('people')};
     return formData;
 }
@@ -28,10 +34,35 @@ const updateOutput = function(tip, amount) {
     document.getElementById("person-total").textContent = `$${amount}`
 }
 
+document.getElementById('tip-custom').addEventListener('focus', (event) => {
+    const customTip = event.target.value;
+    const relatedTarget = event.relatedTarget;
+    if(customTip === 'custom' && relatedTarget.id !== 'tip-custom-input') {
+        document.getElementById('tip-custom-input').focus();
+    }
+});
+document.getElementById('tip-custom-input').addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowLeft' && event.target.selectionStart === 0) {
+        const tip50 = document.getElementById('tip-50');
+        tip50.focus();
+        tip50.checked = true;
+        event.target.value = '';
+    }
+});
+
+
+document.querySelector(('button[type="reset"]')).addEventListener('click', () => {
+    const personTip = document.getElementById("person-tip");
+    const personTotal = document.getElementById("person-total");
+    personTip.textContent = "$0.00";
+    personTotal.textContent = "$0.00";
+});
+
+
 const allInputs = document.querySelectorAll('input');
 for(inputEntry of allInputs){
-    inputEntry.addEventListener('change', () => {
-        const formData = getInfo();
+    inputEntry.addEventListener('change', (event) => {
+        const formData = getInfo(event);
         const result = calculate(formData.bill, formData.tip, formData.people);
         if (result === "Invalid input") {
             return;
